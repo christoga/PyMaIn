b3ac88731f2a74792e3d7d8544f27e340413ae55
Andre Christoga <andrechristoga@gmail.com>
2015-12-10 17:34:59 +0700

    Added setup tools

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from setup
+
+import os
+import setuptools
+
+def plus(os):
+	os.system("plus.py")
+def minus(os):
+	os.system("minus.py")
+def divide(os):
+	os.system("divide.py")
+def multi(os):
+	os.system("multi.py")
+def modulos(os):
+	os.system("modulos.py")
