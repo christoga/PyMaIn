4813f8262e6bdcc74b74be44b5b296ae777e5928
Andre Christoga <andrechristoga@gmail.com>
2015-12-12 00:01:26 +0700

    Remove space between code

--- setup.py
+++ setup.py
@@ -6,15 +6,11 @@ input = raw_input("> (eg plus, minus, divide...)")
 
 if input == "plus":
 	os.system("pymain/plus.py")
-
 if input == "minus":
 	os.system("pymain/minus.py")
-
 if input == "multi":
 	os.system("pymain/multi.py")
-
 if input == "divide":
 	os.system("pymain/divide.py")
-
 if input == "modulos":
 	os.system("pymain/modulos.py")
