aab0b49f148d0fec18ec5d0faad42bcb0108439f
Andre Christoga <andrechristoga@gmail.com>
2015-12-11 14:35:36 +0700

    Remove everything except shebang

--- setup.py
+++ setup.py
@@ -1,13 +1,1 @@
 #!/usr/bin/python
-import os
-
-def plus(os):
-	os.system("plus.py")
-def minus(os):
-	os.system("minus.py")
-def divide(os):
-	os.system("divide.py")
-def multi(os):
-	os.system("multi.py")
-def modulos(os):
-	os.system("modulos.py")
